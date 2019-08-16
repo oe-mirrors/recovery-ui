e1dcc095e783838b6329661d565aa78145f16757
Andreas Oberritter <obi@opendreambox.org>
2019-08-16 19:46:31 +0200

    lcd: Add support for framebuffer-based LCD, rotation and mirroring

--- lcd.c
+++ lcd.c
@@ -28,9 +28,20 @@
 
 #define ARRAY_SIZE(x)	(sizeof((x)) / sizeof(*(x)))
 
+#define LCD_REVERSE_X	(1 << 0)
+#define LCD_REVERSE_Y	(1 << 1)
+#define LCD_INV_AXIS	(1 << 2)
+
 struct color {
 	unsigned int offset;
 	unsigned int size;
+};
+
+struct lcd_rect {
+	int x;
+	int y;
+	unsigned int width;
+	unsigned int height;
 };
 
 struct lcd {
@@ -49,6 +60,8 @@ struct lcd {
 		uint16_t *data16;
 		uint32_t *data32;
 	};
+	bool mapped;
+	unsigned int flags;
 	union {
 		void *background;
 		uint8_t *background8;
@@ -144,7 +157,8 @@ bool lcd_update(struct lcd *lcd)
 {
 	ssize_t ret;
 
-	if (lcd->type == DISPLAY_TYPE_OLED) {
+	if (!lcd->mapped) {
+		lseek(lcd->fd, SEEK_SET, 0);
 		ret = write(lcd->fd, lcd->data, lcd->size);
 		assert(ret == (ssize_t)lcd->size);
 		if (ret < 0) {
@@ -163,12 +177,18 @@ bool lcd_update(struct lcd *lcd)
 
 unsigned int lcd_width(struct lcd *lcd)
 {
-	return lcd->width;
+	if (lcd->flags & LCD_INV_AXIS)
+		return lcd->height;
+	else
+		return lcd->width;
 }
 
 unsigned int lcd_height(struct lcd *lcd)
 {
-	return lcd->height;
+	if (lcd->flags & LCD_INV_AXIS)
+		return lcd->width;
+	else
+		return lcd->height;
 }
 
 static inline unsigned int lcd_scale_factor(struct lcd *lcd)
@@ -186,6 +206,48 @@ unsigned int lcd_font_height(struct lcd *lcd)
 	return 8 * lcd_scale_factor(lcd);
 }
 
+static inline int lcd_x(struct lcd *lcd)
+{
+	if (lcd->flags & LCD_REVERSE_X)
+		return lcd->width - lcd->x - 1;
+	else
+		return lcd->x;
+}
+
+static inline int lcd_y(struct lcd *lcd)
+{
+	if (lcd->flags & LCD_REVERSE_Y)
+		return lcd->height - lcd->y - 1;
+	else
+		return lcd->y;
+}
+
+static inline bool lcd_valid_x(struct lcd *lcd)
+{
+	return lcd->x >= 0 && (size_t)lcd->x < lcd->width;
+}
+
+static inline bool lcd_valid_y(struct lcd *lcd)
+{
+	return lcd->y >= 0 && (size_t)lcd->y < lcd->height;
+}
+
+static inline bool lcd_valid_pos(struct lcd *lcd)
+{
+	return lcd_valid_x(lcd) && lcd_valid_y(lcd);
+}
+
+static inline void lcd_move_cursor(struct lcd *lcd, int cols, int rows)
+{
+	if (lcd->flags & LCD_INV_AXIS) {
+		lcd->x += rows;
+		lcd->y += cols;
+	} else {
+		lcd->x += cols;
+		lcd->y += rows;
+	}
+}
+
 static void lcd_putc_4bpp(struct lcd *lcd, char c)
 {
 	unsigned int row, column, data_index, font_index;
@@ -193,22 +255,22 @@ static void lcd_putc_4bpp(struct lcd *lcd, char c)
 	unsigned int font_height = lcd_font_height(lcd);
 	unsigned int mask;
 
-	mask = (lcd->x & 1) ? 0x0f : 0xf0;
 	font_index = (unsigned char)c * font_width;
 	for (column = 0; column < font_width; column++) {
-		if (lcd->x >= 0 && (size_t)lcd->x < lcd->width) {
-			data_index = lcd->y * lcd->stride + lcd->x * lcd->bpp / 8;
-			for (row = 0; row < font_height; row++) {
+		for (row = 0; row < font_height; row++) {
+			if (lcd_valid_pos(lcd)) {
+				mask = (lcd_x(lcd) & 1) ? 0x0f : 0xf0;
+				data_index = lcd_y(lcd) * lcd->stride + lcd_x(lcd) * lcd->bpp / 8;
 				if (lcdfont[font_index] & (1 << row))
 					lcd->data8[data_index] |= mask;
 				else
 					lcd->data8[data_index] &= ~mask;
-				data_index += lcd->stride;
 			}
+			lcd_move_cursor(lcd, 0, 1);
 		}
-		mask ^= 0xff;
+		lcd_move_cursor(lcd, 0, -font_height);
 		font_index++;
-		lcd->x++;
+		lcd_move_cursor(lcd, 1, 0);
 	}
 }
 
@@ -221,18 +283,19 @@ static void lcd_putc_16bpp(struct lcd *lcd, char c)
 
 	font_index = (unsigned char)c * font_width;
 	for (column = 0; column < font_width; column++) {
-		if (lcd->x >= 0 && (size_t)lcd->x < lcd->width) {
-			data_index = lcd->y * lcd->stride + lcd->x * lcd->bpp / 8;
-			for (row = 0; row < font_height; row++) {
+		for (row = 0; row < font_height; row++) {
+			if (lcd_valid_pos(lcd)) {
+				data_index = lcd_y(lcd) * lcd->stride + lcd_x(lcd) * lcd->bpp / 8;
 				if (lcdfont[font_index / scale_factor] & (1 << (row / scale_factor)))
 					lcd->data16[data_index / 2] = lcd->fgcolor;
 				else
 					lcd->data16[data_index / 2] = lcd->background16[data_index / 2];
-				data_index += lcd->stride;
 			}
+			lcd_move_cursor(lcd, 0, 1);
 		}
+		lcd_move_cursor(lcd, 0, -font_height);
 		font_index++;
-		lcd->x++;
+		lcd_move_cursor(lcd, 1, 0);
 	}
 }
 
@@ -245,18 +308,19 @@ static void lcd_putc_32bpp(struct lcd *lcd, char c)
 
 	font_index = (unsigned char)c * font_width;
 	for (column = 0; column < font_width; column++) {
-		if (lcd->x >= 0 && (size_t)lcd->x < lcd->width) {
-			data_index = lcd->y * lcd->stride + lcd->x * lcd->bpp / 8;
-			for (row = 0; row < font_height; row++) {
+		for (row = 0; row < font_height; row++) {
+			if (lcd_valid_pos(lcd)) {
+				data_index = lcd_y(lcd) * lcd->stride + lcd_x(lcd) * lcd->bpp / 8;
 				if (lcdfont[font_index / scale_factor] & (1 << (row / scale_factor)))
 					lcd->data32[data_index / 4] = lcd->fgcolor;
 				else
 					lcd->data32[data_index / 4] = lcd->background32[data_index / 4];
-				data_index += lcd->stride;
 			}
+			lcd_move_cursor(lcd, 0, 1);
 		}
+		lcd_move_cursor(lcd, 0, -font_height);
 		font_index++;
-		lcd->x++;
+		lcd_move_cursor(lcd, 1, 0);
 	}
 }
 
@@ -296,15 +360,14 @@ int lcd_printf(struct lcd *lcd, const char *fmt, ...)
 	return ret;
 }
 
-struct lcd *hdmi_open(void)
+static struct lcd *fbdev_open(const char *device, unsigned int flags, bool mapped)
 {
-	struct lcd *lcd = NULL;
-	const char device[] = "/dev/fb0";
 	const char tty[] = "/dev/tty0";
 	struct fb_var_screeninfo var;
 	struct fb_fix_screeninfo fix;
-	size_t size;
+	struct lcd *lcd;
 	void *buffer;
+	size_t size;
 	int fd;
 
 	fd = open(tty, O_RDWR | O_CLOEXEC);
@@ -347,16 +410,28 @@ struct lcd *hdmi_open(void)
 
 	size = fix.line_length * var.yres;
 
-	buffer = mmap(NULL, fix.line_length * var.yres_virtual, PROT_WRITE, MAP_SHARED, fd, 0);
-	if (buffer == MAP_FAILED) {
-		perror("mmap");
-		close(fd);
-		return NULL;
+	if (mapped) {
+		buffer = mmap(NULL, fix.line_length * var.yres_virtual, PROT_WRITE, MAP_SHARED, fd, 0);
+		if (buffer == MAP_FAILED) {
+			perror("mmap");
+			close(fd);
+			return NULL;
+		}
+	} else {
+		buffer = malloc(fix.line_length * var.yres_virtual);
+		if (buffer == NULL) {
+			perror("malloc");
+			close(fd);
+			return NULL;
+		}
 	}
 
 	lcd = malloc(sizeof(struct lcd) + size);
 	if (lcd == NULL) {
-		munmap(buffer, size);
+		if (mapped)
+			munmap(buffer, size);
+		else
+			free(buffer);
 		close(fd);
 		return NULL;
 	}
@@ -368,6 +443,8 @@ struct lcd *hdmi_open(void)
 	lcd->bpp = var.bits_per_pixel;
 	lcd->stride = fix.line_length;
 	lcd->data = buffer + lcd->stride * var.yoffset;
+	lcd->mapped = mapped;
+	lcd->flags = flags;
 
 	lcd->red.offset = var.red.offset;
 	lcd->red.size = var.red.length;
@@ -378,7 +455,6 @@ struct lcd *hdmi_open(void)
 	lcd->alpha.offset = var.transp.offset;
 	lcd->alpha.size = var.transp.length;
 
-	lcd->type = DISPLAY_TYPE_HDMI;
 	lcd->size = size;
 	lcd->background = (unsigned char *)&lcd[1];
 	memset(lcd->background, 0, size);
@@ -387,9 +463,30 @@ struct lcd *hdmi_open(void)
 	return lcd;
 }
 
-struct lcd *lcd_open(void)
+static struct lcd *hdmi_open(void)
 {
-	const char device[] = "/dev/dbox/oled0";
+	const char tty[] = "/dev/tty0";
+	int fd;
+
+	fd = open(tty, O_RDWR | O_CLOEXEC);
+	if (fd < 0) {
+		fprintf(stderr, "lcd: can't open %s: %m\n", tty);
+		return NULL;
+	}
+
+	if (ioctl(fd, KDSETMODE, KD_GRAPHICS) < 0) {
+		perror("KDSETMODE");
+		close(fd);
+		return NULL;
+	}
+
+	close(fd);
+
+	return fbdev_open("/dev/fb0", 0, true);
+}
+
+static struct lcd *stb_lcd_open(const char *device)
+{
 	const unsigned int width = ulong_from_file("/proc/stb/lcd/xres", 128);
 	const unsigned int height = ulong_from_file("/proc/stb/lcd/yres", 64);
 	const unsigned int bpp = ulong_from_file("/proc/stb/lcd/bpp", 4);
@@ -449,6 +546,20 @@ struct lcd *lcd_open(void)
 	return lcd;
 }
 
+static struct lcd *lcd_open(void)
+{
+	const char oleddev[] = "/dev/dbox/oled0";
+	const char fbdev[] = "/dev/fb3";
+
+	if (access(oleddev, W_OK) == 0)
+		return stb_lcd_open(oleddev);
+
+	if (access(fbdev, W_OK) == 0)
+		return fbdev_open(fbdev, LCD_INV_AXIS | LCD_REVERSE_Y, false);
+
+	return NULL;
+}
+
 struct lcd *display_open(enum display_type type)
 {
 	struct lcd *lcd = NULL;
@@ -459,6 +570,8 @@ struct lcd *display_open(enum display_type type)
 		lcd = hdmi_open();
 
 	if (lcd != NULL) {
+		lcd->type = type;
+
 		if (lcd->width == 128 && lcd->bpp == 4) {
 			lcd->logo = lcdlogo_128x8_gray4;
 			lcd->logo_size = sizeof(lcdlogo_128x8_gray4);
@@ -470,7 +583,7 @@ struct lcd *display_open(enum display_type type)
 			}
 			lcd->logo = lcdlogo_400x240_rgb565;
 			lcd->logo_size = sizeof(lcdlogo_400x240_rgb565);
-		} else {
+		} else if (lcd->width >= lcd->height) {
 			lcd->logo = lcdlogo_96x7_mono;
 			lcd->logo_size = sizeof(lcdlogo_96x7_mono);
 		}
@@ -488,27 +601,69 @@ void lcd_release(struct lcd *lcd)
 
 void lcd_set_x(struct lcd *lcd, int x)
 {
-	lcd->x = x;
+	if (lcd->flags & LCD_INV_AXIS)
+		lcd->y = x;
+	else
+		lcd->x = x;
 }
 
 void lcd_set_y(struct lcd *lcd, int y)
 {
-	lcd->y = y;
+	if (lcd->flags & LCD_INV_AXIS)
+		lcd->x = y;
+	else
+		lcd->y = y;
 }
 
-void lcd_clear(struct lcd *lcd, unsigned int height)
+static void lcd_blit(struct lcd *lcd, void *dst, const void *src, struct lcd_rect *rect)
 {
-	int y = lcd->y;
+	size_t y;
 
-	if (y < 0) {
-		height += y;
-		y = 0;
+	if (lcd->flags & LCD_REVERSE_X)
+		rect->x = lcd->width - rect->x - rect->width;
+	if (lcd->flags & LCD_REVERSE_Y)
+		rect->y = lcd->height - rect->y - rect->height;
+
+	if (rect->x < 0) {
+		rect->width += rect->x;
+		rect->x = 0;
 	}
-	if (y + height > lcd->height)
-		height = lcd->height - y;
+	if (rect->y < 0) {
+		rect->height += rect->y;
+		rect->y = 0;
+	}
 
-	if ((int)height > 0)
-		memcpy(&lcd->data8[lcd->stride * y], &lcd->background8[lcd->stride * y], lcd->stride * height);
+	if (rect->x + rect->width > lcd->width)
+		rect->width = lcd->width - rect->x;
+	if (rect->y + rect->height > lcd->height)
+		rect->height = lcd->height - rect->y;
+
+	if (rect->width <= 0)
+		return;
+
+	for (y = rect->y; y < rect->y + rect->height; y++) {
+		size_t offset = lcd->stride * y + rect->x * lcd->bpp / 8;
+		memcpy(dst + offset, src + offset, rect->width * lcd->bpp / 8);
+	}
+}
+
+void lcd_clear(struct lcd *lcd, unsigned int lines)
+{
+	struct lcd_rect rect;
+
+	if (lcd->flags & LCD_INV_AXIS) {
+		rect.x = lcd->x;
+		rect.y = 0;
+		rect.width = lines;
+		rect.height = lcd->height;
+	} else {
+		rect.x = 0;
+		rect.y = lcd->y;
+		rect.width = lcd->width;
+		rect.height = lines;
+	}
+
+	lcd_blit(lcd, lcd->data, lcd->background, &rect);
 }
 
 static ssize_t lcd_write(struct lcd *lcd, const void *buf, size_t count)
@@ -550,6 +705,9 @@ void lcd_save_background(struct lcd *lcd)
 
 void lcd_write_logo(struct lcd *lcd)
 {
+	if (!(lcd->logo && lcd->logo_size))
+		return;
+
 	if (lcd->bpp == 16 && (lcd->width != 400 || lcd->height != 240)) {
 		unsigned int scale_factor = lcd_scale_factor(lcd);
 		unsigned char logo[lcd->logo_size * 16 * scale_factor];
